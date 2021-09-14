b5510190cce41ab6080be3ef658543be71d3b860
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-09-14 15:04:11 +1000

    Setup Auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AuthContext = React.createContext()
+export function useAuth() {
+    return useContext(AuthContext)
+}
+export function AuthProvider( { children } ) {
+    const [currentUser, setCurrentUser] = useState()
+    
+    const value = {
+        currentUser
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
